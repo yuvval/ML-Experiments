eff10f95befbb7a9788bd24574ed0e50ad31bd94
yuval <yuval@ctx>
2014-12-16 08:29:29 +0200

    find best (maximizing) hyper param

--- search_hyper_params.m
+++ search_hyper_params.m
@@ -12,7 +12,7 @@ function [best_hyper_param_id, results_fnames] = search_hyper_params(search_para
 
 
     %% set indicator that we train for searching hyper param
-    search_params.cfg_params.train_objective = 'hpsearch';
+    search_params.cfg_params.train_aim = 'hpsearch';
 
     %% load the dataset
     [all_examples, all_labels, search_params.cfg_params] = search_params.load_data_func(search_params.cfg_params);
@@ -23,27 +23,32 @@ function [best_hyper_param_id, results_fnames] = search_hyper_params(search_para
     num_examples = length(labels);
     
     %% Split to K folds (training / validation)
-    folds = cvpartition(num_examples, 'KFold', search_params.kfolds);
+    ifolds = cvpartition(num_examples, 'KFold', search_params.kfolds);
     
     %% prepare combinations of hyper_params and folds to train upon
     
-    train_params_comb = allcomb(1:search_params.kfolds, search_params.hyper_params_indices).';
+    train_params_comb = allcomb(search_params.hyper_params_indices, 1:search_params.kfolds).';
     
     %% iterate (train) on all hyper_params and folds
     
     search_result_criteria = zeros(size(train_params_comb,2),1);
     % todo change to parfor
-    for comb_id = size(train_params_comb,2)
-        fold_id = train_params_comb(1,comb_id);
-        hyper_param_id = train_params_comb(2,comb_id);        
-        search_params.cfg_params.fold_id = fold_id;
-        search_params.cfg_params.dataset_fold_id = search_params.dataset_fold_id;
-        search_result_criteria(comb_id) = search_params.train_func(hyper_param_id, search_params.cfg_params, examples, labels, folds.training(fold_id));
+    for comb_id = 1:length(search_result_criteria)
+        hyper_param_id = train_params_comb(1,comb_id);        
+        fold_id = train_params_comb(2,comb_id);
+        search_params.cfg_params.ifold_id = fold_id;
+        search_params.cfg_params.ofold_id = search_params.dataset_fold_id;
+        search_result_criteria(comb_id) = search_params.train_func(hyper_param_id, search_params.cfg_params, examples, labels, ifolds.training(fold_id));
     end
     
-    %% Todo: find best hyper param
-    
-    best_hyper_param_id = -1;   
+    %% Todo: find best (maximizing) hyper param
+
+    iter = 1; avg_crit_per_hp = zeros(size(search_params.hyper_params_indices));
+    for id = search_params.hyper_params_indices
+        avg_crit_per_hp(iter) = mean(search_result_criteria(train_params_comb(1,:) == id));
+        iter = iter+1;
+    end
+    best_hyper_param_id = search_params.hyper_params_indices(avg_crit_per_hp==max(avg_crit_per_hp));   
 
 
     
