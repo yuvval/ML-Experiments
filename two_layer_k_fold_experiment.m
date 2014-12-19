3b7d2c43b4af60659ff475a1abbc34f57be46513
yuval <yuval@ctx>
2014-12-19 02:44:56 +0200

    documentation

--- two_layer_k_fold_experiment.m
+++ two_layer_k_fold_experiment.m
@@ -1,4 +1,29 @@
 function results = two_layer_k_fold_experiment(experiment_params, model_cfg_params)
+% function results = two_layer_k_fold_experiment(experiment_params, model_cfg_params)
+% Runs a 2 layer K folds experiment.
+% input params: experiment params, model_configuration params
+% returns: results structure with the following fields:
+%             results.fnames = filenames cell array of each fold trial (with the best hyper param)
+%             results.best_hyprm_id = best_hyprm_id for each fold 
+%             results.criteria = criteria array for each fold
+%             results.best_hyprm_max_steps_num = best_hyprm_max_steps_num during hyper param grid search (this value is used for the training of the outer fold experiment with best hyper param)
+%             results.search_results = cell array with the results history of hyper param grid search
+%
+% usage example:
+% 
+%             experiment_params.load_data_func = @load_dataset; % syntax: [examples, labels, model_cfg_params] = load_dataset(model_cfg_params)
+%             experiment_params.train_func = @similarity_train; % syntax: [result_criterion, full_fname_results, steps_num] = similarity_train(hyper_param_id, model_cfg_params, examples, labels, training_fold_logical_index, ofold, ifold, steps_num)
+%             experiment_params.kfolds = kfolds;
+%             experiment_params.hyper_params_indices = hyper_params_indices to iterate upon
+%             experiment_params.experiment_results_ref_fname = ['similarity_experiment_' method_params_str '_' dataset_params_str '_githash_' git_commit_hash];
+%             experiment_params.path_results_mat = '/cortex/yuvval/results_mat/experiments';
+%             % additional non mandatory params (for history)
+%             experiment_params.git_commit_hash = git_commit_hash;
+%
+%             set model_cfg_params according to the requirements of your similarity_training function
+%
+
+
     %% init
     kfolds = experiment_params.kfolds;
     hyper_params_indices = experiment_params.hyper_params_indices;
